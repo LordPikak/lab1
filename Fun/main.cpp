7900228f4df9a27e216606ad4d7e27af404c308c
LordPikak <wpiecuch012@gmail.com>
2020-10-10 11:53:04 +0200

    Update main.cpp

--- Fun/main.cpp
+++ Fun/main.cpp
@@ -4,6 +4,11 @@ int main()
 {
     int x;
     std::cin >> x;
-    std::cout << x;
+    std::cout << x << std::endl;
+    if (x%2==0)std::cout<<"Your number is even.";
+    else std::cout<<"Your number is odd.";
+
+
+
     return 0;
 }
